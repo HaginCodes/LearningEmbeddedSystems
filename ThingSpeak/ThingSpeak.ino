23ce0ba4a1bff8519f568e333d78b2d74581435d
Hagin <hagincubes@gmail.com>
2017-07-26 16:36:18 -0400

    Working on thinkspeak configuration both being server and client.

--- ThingSpeak/ThingSpeak.ino
+++ ThingSpeak/ThingSpeak.ino
@@ -6,8 +6,8 @@
 #include <ThingSpeak.h>
 
 
-long unsigned long myChannelNumber = 49494949; //Your channel num
-const char * myWriteAPIKey = "LKJFLSDFJLNVMLK43l210LA"; //Your API Key
+long unsigned long myChannelNumber = ; //Your channel num
+const char * myWriteAPIKey = "; //Your API Key
 
 
 #include <SPI.h>
@@ -16,29 +16,15 @@ const char * myWriteAPIKey = "LKJFLSDFJLNVMLK43l210LA"; //Your API Key
 #define USE_WIFI101_SHIELD
 //#define USE_ETHERNET_SHIELD
 
-#if defined(ARDUINO_AVR_YUN)
-#include "YunClient.h"
-YunClient client;
-#else
-#if defined(USE_WIFI101_SHIELD) || defined(ARDUINO_SAMD_MKR1000) || defined(ARDUINO_ARCH_ESP8266)
-// Use WiFi
-#ifdef ARDUINO_ARCH_ESP8266
-#include <ESP8266WiFi.h>
-#else
 #include <SPI.h>
-#endif
-char ssid[] = "FreshAir";    //  your network SSID (name)
-char pass[] = "OnMyHonor";   // your network password
+
+char ssid[] = "";    //  your network SSID (name)
+char pass[] = "";   // your network password
 int status = WL_IDLE_STATUS;
 WiFiClient  client;
-#elif defined(USE_ETHERNET_SHIELD)
+WiFiServer server(80);
 // Use wired ethernet shield
-#include <SPI.h>
-#include <Ethernet.h>
-byte mac[] = { 0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
-EthernetClient client;
-#endif
-#endif
+
 
 #ifdef ARDUINO_ARCH_AVR
 // On Arduino:  0 - 1023 maps to 0 - 5 volts
@@ -62,30 +48,87 @@ EthernetClient client;
 
 void setup() {
 
-#ifdef ARDUINO_AVR_YUN
-  Bridge.begin();
-#else
-#if defined(ARDUINO_ARCH_ESP8266) || defined(USE_WIFI101_SHIELD) || defined(ARDUINO_SAMD_MKR1000)
-  WiFi.begin(ssid, pass);
-#else
-  Ethernet.begin(mac);
-#endif
-#endif
 
+  Serial.begin(9600);
+  while(!Serial) {
+    ;
+  }
+  WiFi.begin(ssid, pass);
+  Serial.println("Wifi connected: ");
+  Serial.println(ssid);
+
+  //Start Server
+  server.begin();
+
+  //Start ThingSpeak
   ThingSpeak.begin(client);
+  Serial.println();
+  Serial.println("Set up complete");
 }
 
 void loop() {
-  // read the input on analog pin 0:
+
+
+  WiFiClient client = server.available();
+  if(client) {
+    Serial.println("Client successfully created");
+
+    boolean currentLineIsBlank = true;
+    while(client.connected()){
+      if(client.available()){
+        char c = client.read();
+        Serial.write(c);
+
+        if(c == '\n' && currentLineIsBlank){
+
+          //send a response header
+          client.println("HTTP/1.1 200 OK");
+          client.println("Content-Type: text/html");
+          client.println("Connection: close");  // the connection will be closed after completion of the response
+          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
+          client.println();
+          client.println("<!DOCTYPE HTML>");
+          client.println("<html>");
+          //print out the analog input pin on the arduino.. I think
+          // output the value of each analog input pin
+          for (int analogChannel = 0; analogChannel < 6; analogChannel++) {
+            int sensorReading = analogRead(analogChannel);
+            client.print("analog input ");
+            client.print(analogChannel);
+            client.print(" is ");
+            client.print(sensorReading);
+            client.println("<br />");       
+          }
+          client.println("</html>");
+           break;
+        }
+        if (c == '\n') {
+          // you're starting a new line
+          currentLineIsBlank = true;
+        } 
+        else if (c != '\r') {
+          //character on that line bud
+          currentLineIsBlank = false;
+        }
+      }
+    }
+    // give the web browser time to receive the data
+    delay(1);
+    
+    // close the connection:
+  //  client.stop();
+   // Serial.println("client disonnected");
+    }
+
+    // read the input on analog pin 0:
   int sensorValue = analogRead(A0);
   // Convert the analog reading
-  // On Uno,Mega,YunArduino:  0 - 1023 maps to 0 - 5 volts
-  // On ESP8266:  0 - 1023 maps to 0 - 1 volts
   // On MKR1000,Due: 0 - 4095 maps to 0 - 3.3 volts
   float voltage = sensorValue * (VOLTAGE_MAX / VOLTAGE_MAXCOUNTS);
   Serial.println(voltage);
   // Write to ThingSpeak. There are up to 8 fields in a channel, allowing you to store up to 8 different
   // pieces of information in a channel.  Here, we write to field 1.
-  ThingSpeak.writeField(myChannelNumber, 1, voltage, myWriteAPIKey);
-  delay(20000); // ThingSpeak will only accept updates every 15 seconds.
-}
+   ThingSpeak.writeField(myChannelNumber, 1, voltage, myWriteAPIKey);
+  delay(15000); // ThingSpeak will only accept updates every 15 seconds.
+   }
+
